37824fec80fa90c8fdcbed4aa269a83a1ab3a46c
Kristian Mitchell <kmitch79@mail.ccsf.edu>
2025-09-24 21:55:34 -0700

    Initial Commit

---
+++ Discussion - Module 5 Credit card fraud/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Discussion - Module 5 Credit card fraud
+//
+//  Created by Kristian Mitchell on 9/24/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
